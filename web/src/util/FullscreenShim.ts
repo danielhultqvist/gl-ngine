3541e72a010b0ac7841de776deb110e2fcfca16e
Daniel Hultqvist <daniel@typedef.se>
2018-12-13 22:18:10 +0100

    Update JS dependencies to latest version

--- web/src/util/FullscreenShim.ts
+++ web/src/util/FullscreenShim.ts
@@ -2,8 +2,6 @@ export function goFullscreen(element: HTMLElement) {
   const tmpElement: any = element;
   if (element.requestFullscreen) {
     element.requestFullscreen();
-  } else if (element.webkitRequestFullScreen) {
-    element.webkitRequestFullScreen();
   } else if (tmpElement.mozRequestFullScreen) {
     tmpElement.mozRequestFullScreen();
   } else if (tmpElement.msRequestFullscreen) {
